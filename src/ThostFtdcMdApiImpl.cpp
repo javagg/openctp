259355beb089ab19c5daafa40c181a11cb3a8fa5
李璐 <alex@AlexMacBook.local>
2014-07-31 16:00:36 +0800

    first commit

---
+++ src/ThostFtdcMdApiImpl.cpp
@@ -0,0 +1,11 @@
+#include "ThostFtdcMdApi.h"
+
+CThostFtdcMdApi *CThostFtdcMdApi::CreateFtdcMdApi(const char *pszFlowPath, const bool bIsUsingUdp, const bool bIsMulticast)
+{
+    return 0;
+}
+
+class CThostFtdcMdApiImpl : public CThostFtdcMdApi
+{
+
+};
---
+++ src/ThostFtdcTraderApiImpl.cpp
@@ -0,0 +1,11 @@
+#include "ThostFtdcTraderApi.h"
+
+CThostFtdcTraderApi *CThostFtdcTraderApi::CreateFtdcTraderApi(const char *pszFlowPath)
+{
+    return 0;
+}
+
+class CThostFtdcTraderApiImpl : public CThostFtdcTraderApi
+{
+
+};
